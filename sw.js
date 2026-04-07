de1951687d04deeaf2956f286f4a5cb39389fd2e
tikof1980 <serykouame@gmail.com>
2026-04-07 20:33:21 +0000

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='wamarket-v1';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json'])));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const clone=r.clone();caches.open(CACHE).then(c=>{if(e.request.method==='GET')c.put(e.request,clone);});return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
