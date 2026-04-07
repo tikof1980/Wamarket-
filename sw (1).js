2fe57a84c3ea601003df0d74a9a558dca5d4bf15
tikof1980 <serykouame@gmail.com>
2026-04-07 13:43:16 +0000

    Delete sw (1).js

--- sw (1).js
+++
@@ -1,5 +0,0 @@
-const CACHE='wamarket-cache-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(r=>{const clone=r.clone();caches.open(CACHE).then(c=>{if(e.request.method==='GET')c.put(e.request,clone);});return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
